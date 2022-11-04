26914e2f402218a52134eaf30348ffcf708e945f
NatSof <natariie.studie@gmail.com>
2022-11-04 12:19:31 +0100

    centered window and added margin

--- src/ChatPanel.java
+++ src/ChatPanel.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class ChatPanel {
 	private JPanel panel1;
 	private JTextPane textPane1;
+	private JPanel ChatJPanel;
+	private JLabel TextJLabel;
 
 
 	public JPanel getPanel() {
